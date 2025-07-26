cde441adbf798c54d14a92821d9f34d52e66a4e9
vivek-sahane <viveksahane1528@gmail.com>
2025-07-26 11:42:32 +0530

    coverImage error solved

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,7 +19,8 @@ import fs from "fs"
                 resource_type: "auto"
             })
             // file has been uploded successfully
-            console.log("file is uploaded on cloudinary",response.url);
+            //console.log("file is uploaded on cloudinary",response.url);
+            fs.unlinkSync(localFilePath)
             return response;
 
         } catch (error) {
